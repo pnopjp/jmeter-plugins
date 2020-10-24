0bc794235f6e806cefa167bd8159e8ca7f968630
Kuniteru Asami <asami@pnop.co.jp>
2020-10-24 19:03:48 +0900

    AzAdAccessToken: support proxy wthout auth

--- plugins/functions/src/main/java/jp/co/pnop/jmeter/functions/AzAdAccessToken.java
+++ plugins/functions/src/main/java/jp/co/pnop/jmeter/functions/AzAdAccessToken.java
@@ -5,15 +5,23 @@ import org.apache.jmeter.functions.AbstractFunction;
 import org.apache.jmeter.functions.InvalidVariableException;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
@@ -81,6 +89,22 @@ public class AzAdAccessToken extends AbstractFunction {
         try {
             CloseableHttpClient httpclient = HttpClients.createDefault();
             HttpPost request = new HttpPost("https://login.microsoftonline.com/" + URLEncoder.encode(tenantId, "UTF-8").replace("+", "%20") + "/oauth2" + aadVersion + "/token");
+            
+            // proxy
+            String proxyHost = JMeterUtils.getPropDefault("https.proxyHost", "").trim();
+            if (!proxyHost.isEmpty()) {
+                int proxyPort = Integer.parseInt(JMeterUtils.getPropDefault("https.proxyPort", "8080").trim());
+                String nonProxyHosts = JMeterUtils.getPropDefault("https.nonProxyHost", "").trim();
+                /* Auth
+                String proxyUser = JMeterUtils.getPropDefault("http.proxyUser", "").trim();
+                String proxyPass = URLEncoder.encode(JMeterUtils.getPropDefault("http.proxyPass", ""), "UTF-8");
+                if (proxyUser.length() > 0) {
+                }
+                */
+                HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+                RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+                request.setConfig(config);
+            }
 
             request.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
