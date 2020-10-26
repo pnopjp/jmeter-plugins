95ba97c467d6c19967d9a48236d0d7c4bd6e1256
Kuniteru Asami <asami@pnop.co.jp>
2020-10-27 01:27:07 +0900

    small refactoring

--- plugins/functions/src/main/java/jp/co/pnop/jmeter/functions/AzAppServiceAuthenticationToken.java
+++ plugins/functions/src/main/java/jp/co/pnop/jmeter/functions/AzAppServiceAuthenticationToken.java
@@ -11,7 +11,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.entity.StringEntity;
 
