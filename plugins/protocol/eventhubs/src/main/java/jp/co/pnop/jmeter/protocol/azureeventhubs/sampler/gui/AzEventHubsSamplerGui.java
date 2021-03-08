588aa032b21d2c976fa95f85cec7525fa16880cc
Kuniteru Asami <asami@pnop.co.jp>
2021-03-08 23:44:43 +0900

    Organized the files layout

--- plugins/protocol/eventhubs/src/main/java/jp/co/pnop/jmeter/protocol/azureeventhubs/sampler/gui/AzEventHubsSamplerGui.java
+++ plugins/protocol/eventhubs/src/main/java/jp/co/pnop/jmeter/protocol/azureeventhubs/sampler/gui/AzEventHubsSamplerGui.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jp.co.pnop.jmeter.protocol.azureeventhubs.control.gui;
+package jp.co.pnop.jmeter.protocol.azureeventhubs.sampler.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
