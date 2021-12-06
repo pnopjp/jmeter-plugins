8008f9d9874095985dad9b2b148a99c7e3c4cbd6
Kuniteru Asami <asami@pnop.co.jp>
2021-12-06 21:56:11 +0900

    some KeyVault AuthType support and proxy support

--- plugins/AzLoadTestingStub/src/main/java/jp/co/pnop/jmeter/functions/GetSecret.java
+++ plugins/AzLoadTestingStub/src/main/java/jp/co/pnop/jmeter/functions/GetSecret.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.print.attribute.standard.JobHoldUntil;
-
 import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.rest.Response;
 import com.azure.identity.AzureAuthorityHosts;
-import com.azure.identity.ClientCertificateCredential;
+import com.azure.identity.AzureCliCredentialBuilder;
 import com.azure.identity.ClientCertificateCredentialBuilder;
-import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.identity.EnvironmentCredentialBuilder;
-import com.azure.identity.InteractiveBrowserCredential;
 import com.azure.identity.InteractiveBrowserCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.identity.VisualStudioCodeCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
@@ -30,6 +28,8 @@ import org.apache.jorphan.util.JOrphanUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import jp.co.pnop.jmeter.util.httpclient.AzUtilHttpClient;
 
 public class GetSecret extends AbstractFunction {
     
@@ -58,7 +58,11 @@ public class GetSecret extends AbstractFunction {
     private static final String AUTH_TYPE = "auth_type";
     private static final String CLIENT_SECRET = "client_secret";
     private static final String CLIENT_CERTIFICATE = "client_certificate";
-    private static final String INTERACTIVE_BROWSER = "web_browser";
+    private static final String INTERACTIVE_BROWSER = "interactive_browser";
+    private static final String MANAGED_ID = "managed_id";
+    private static final String AZURE_CLI = "azure_cli";
+    private static final String VS_CODE = "vs_code";
+
     private static final String CERTIFICATE_TYPE = "certificate_type";
     private static final String CERTIFICATE_FILE = "certificate_file";
     private static final String CERTIFICATE_PASSWORD = "certificate_password";
@@ -80,7 +84,18 @@ public class GetSecret extends AbstractFunction {
 
     @Override
     public synchronized String execute(SampleResult previousResult, Sampler currentSampler) throws InvalidVariableException {
-        TokenCredential credential = null;
+        Boolean configThread = false;
+        StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement ste: stElements) {
+            if (ste.getClassName().equals("org.apache.jmeter.config.Argument")) {
+                configThread = true;
+                break;
+            }
+        }
+        if (!configThread) {
+            return "";
+        }
+        
         String secretNameParam = values[PARAM_SECRET_NAME].execute().trim();
         String secret = "";
         String storeType =  JMeterUtils.getPropDefault(new StringBuilder(JMPROPS_GET_SECRET).append(secretNameParam).append(".").append(STORE_TYPE).toString(), "").trim().toLowerCase();
@@ -89,90 +104,7 @@ public class GetSecret extends AbstractFunction {
             switch (storeType) {
                 //// KeyVault
                 case KEY_VAULT:
-                String authType = getKeyVaultProp(secretNameParam, AUTH_TYPE).trim().toLowerCase();
-                String tenantId = getKeyVaultProp(secretNameParam, TENANT_ID);
-                String clientId = getKeyVaultProp(secretNameParam, CLIENT_ID);
-                String authorityHost = getKeyVaultProp(secretNameParam, AUTHORITY_HOST, AzureAuthorityHosts.AZURE_PUBLIC_CLOUD);
-                switch (authorityHost) {
-                    case "AzureChina":
-                    authorityHost = AzureAuthorityHosts.AZURE_CHINA;
-                    break;
-
-                    case "AzureGermany":
-                    authorityHost = AzureAuthorityHosts.AZURE_GERMANY;
-                    break;
-
-                    case "AzureGovernment":
-                    authorityHost = AzureAuthorityHosts.AZURE_GOVERNMENT;
-                    break;
-
-                    case "AzurePublicCloud":
-                    authorityHost = AzureAuthorityHosts.AZURE_PUBLIC_CLOUD;
-                    break;
-                }
-
-                switch (authType) {
-                    //// ClientSecretCredential
-                    case CLIENT_SECRET:
-                    String clientSecret = getKeyVaultProp(secretNameParam, CLIENT_SECRET);
-                    credential = new ClientSecretCredentialBuilder()
-                        .tenantId(tenantId)
-                        .clientId(clientId)
-                        .clientSecret(clientSecret)
-                        .authorityHost(authorityHost)
-                        .build();
-                    break;
-
-                    //// ClientCertificateCredential
-                    case CLIENT_CERTIFICATE:
-                    ClientCertificateCredentialBuilder clientCertificateCredentialBuilder = new ClientCertificateCredentialBuilder()
-                        .tenantId(tenantId)
-                        .clientId(clientId)
-                        .authorityHost(authorityHost);
-                    String clientCertificateType = getKeyVaultProp(secretNameParam, CERTIFICATE_TYPE).trim().toUpperCase();
-                    String certificateFile = getKeyVaultProp(secretNameParam, CERTIFICATE_FILE);
-                    if (clientCertificateType.equals(CLIENT_CERTIFICATE_PEM)) {
-                        clientCertificateCredentialBuilder = clientCertificateCredentialBuilder.pemCertificate(certificateFile);
-                    } else { // CLIENT_CERTIFICATE_PFX
-                        String clientCertificatePassword = getKeyVaultProp(secretNameParam, CERTIFICATE_PASSWORD);
-                        clientCertificateCredentialBuilder = clientCertificateCredentialBuilder.pfxCertificate(certificateFile, clientCertificatePassword);
-                    }
-                    credential = clientCertificateCredentialBuilder.build();
-                    break;
-
-                    //// EnvironmentCredential
-                    case ENVIRONMENT_VARIABLE:
-                    credential = new EnvironmentCredentialBuilder().authorityHost(authorityHost).build();
-                    break;
-
-                    //// ManagedIdCredential
-
-                    //// DeviceCodeCredential
-
-                    //// InteractiveBrowserCredential
-                    case INTERACTIVE_BROWSER:
-                    credential = new InteractiveBrowserCredentialBuilder().authorityHost(authorityHost).build();
-                    break;
-
-                    //// UsernamePasswordCredential
-
-                }
-
-                String vaultUrl = getKeyVaultProp(secretNameParam, VAULT_URI);
-                SecretClient secretClient = new SecretClientBuilder()
-                    .vaultUrl(vaultUrl)
-                    .credential(credential)
-                    .buildClient();
-
-                String secretName = getKeyVaultProp(secretNameParam, SECRET_NAME);
-                String secretVersion = getKeyVaultProp(secretNameParam, SECRET_VERSION);
-                KeyVaultSecret keyVaultSecret;
-                if (JOrphanUtils.isBlank(secretVersion)) {
-                    keyVaultSecret = secretClient.getSecret(secretName);
-                } else {
-                    keyVaultSecret = secretClient.getSecret(secretName, secretVersion);
-                }
-                secret = keyVaultSecret.getValue();
+                secret = getKeyVaultSecret(secretNameParam);
                 break;
 
                 //// jmeter.properties or user.properties
@@ -213,9 +145,140 @@ public class GetSecret extends AbstractFunction {
             
             return secret;
         } catch (GetSecretException ex) {
-            log.error(new StringBuilder("Error calling {} function with Secret name \"{}\". ").append(ex.getMessage()).toString(), KEY, secretNameParam, ex);
-            throw new InvalidVariableException(ex.getCause());
+            log.error("Error calling {} function with Secret name \"{}\". {}", KEY, secretNameParam, ex.getMessage(), ex);
+            return "";
+            //throw new InvalidVariableException(ex.getCause());
+        } catch (Exception ex) {
+            log.error("Error calling {} function with Secret name \"{}\". {}", KEY, secretNameParam, ex.toString(), ex);
+            return "";
         }
+    }
+
+    private String getKeyVaultSecret(String secretNameParam) {
+        TokenCredential credential = getCredential(secretNameParam);
+        if (credential == null) {
+            log.error("Failed to authenticate to Azure Key Vault. __GetSecret({})", secretNameParam);
+            return "";
+        }
+
+        String vaultUrl = getKeyVaultProp(secretNameParam, VAULT_URI);
+        SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl(vaultUrl)
+            .credential(credential)
+            .httpClient(AzUtilHttpClient.httpClientBase())
+            .buildClient();
+
+        String secretName = getKeyVaultProp(secretNameParam, SECRET_NAME);
+        String secretVersion = getKeyVaultProp(secretNameParam, SECRET_VERSION);
+        Response<KeyVaultSecret> response;
+        response = secretClient.getSecretWithResponse(secretName, secretVersion, null);
+        if (response.getStatusCode() != 200) {
+            log.error("Failed to authenticate to Azure Key Vault. [__GetSecret({})]: {}", secretNameParam, response.getValue());
+            return "";
+        }
+        KeyVaultSecret keyVaultSecret = response.getValue();
+
+        return keyVaultSecret.getValue();
+    }
+
+    private TokenCredential getCredential(String secretNameParam) {
+        String authType = getKeyVaultProp(secretNameParam, AUTH_TYPE).trim().toLowerCase();
+        String tenantId = getKeyVaultProp(secretNameParam, TENANT_ID);
+        String clientId = getKeyVaultProp(secretNameParam, CLIENT_ID);
+        String authorityHost = getKeyVaultProp(secretNameParam, AUTHORITY_HOST, AzureAuthorityHosts.AZURE_PUBLIC_CLOUD).trim();
+        switch (authorityHost.toLowerCase()) {
+            case "azurechina":
+            authorityHost = AzureAuthorityHosts.AZURE_CHINA;
+            break;
+
+            case "azuregermany":
+            authorityHost = AzureAuthorityHosts.AZURE_GERMANY;
+            break;
+
+            case "azuregovernment":
+            authorityHost = AzureAuthorityHosts.AZURE_GOVERNMENT;
+            break;
+
+            case "azurepubliccloud":
+            authorityHost = AzureAuthorityHosts.AZURE_PUBLIC_CLOUD;
+            break;
+        }
+
+        switch (authType) {
+            //// ClientSecretCredential
+            case CLIENT_SECRET:
+            String clientSecret = getKeyVaultProp(secretNameParam, CLIENT_SECRET);
+            return new ClientSecretCredentialBuilder()
+                .tenantId(tenantId)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .authorityHost(authorityHost)
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// ClientCertificateCredential
+            case CLIENT_CERTIFICATE:
+            ClientCertificateCredentialBuilder clientCertificateCredentialBuilder = new ClientCertificateCredentialBuilder()
+                .tenantId(tenantId)
+                .clientId(clientId)
+                .authorityHost(authorityHost);
+            String clientCertificateType = getKeyVaultProp(secretNameParam, CERTIFICATE_TYPE).trim().toUpperCase();
+            String certificateFile = getKeyVaultProp(secretNameParam, CERTIFICATE_FILE);
+            switch (clientCertificateType) {
+                case CLIENT_CERTIFICATE_PEM:
+                clientCertificateCredentialBuilder = clientCertificateCredentialBuilder.pemCertificate(certificateFile);
+                break;
+
+                case CLIENT_CERTIFICATE_PFX:
+                String clientCertificatePassword = getKeyVaultProp(secretNameParam, CERTIFICATE_PASSWORD);
+                clientCertificateCredentialBuilder = clientCertificateCredentialBuilder.pfxCertificate(certificateFile, clientCertificatePassword);
+                break;
+            }
+            return clientCertificateCredentialBuilder
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// EnvironmentCredential
+            case ENVIRONMENT_VARIABLE:
+            return new EnvironmentCredentialBuilder()
+                .authorityHost(authorityHost)
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// ManagedIdCredential
+            case MANAGED_ID:
+            ManagedIdentityCredentialBuilder managedIdCredentialBuilder = new ManagedIdentityCredentialBuilder();
+            if (!JOrphanUtils.isBlank(clientId)) {
+                managedIdCredentialBuilder = managedIdCredentialBuilder.clientId(clientId);
+            }
+            return managedIdCredentialBuilder
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// InteractiveBrowserCredential
+            case INTERACTIVE_BROWSER:
+            return new InteractiveBrowserCredentialBuilder()
+                .authorityHost(authorityHost)
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// AzureCliCredential
+            case AZURE_CLI:
+            return new AzureCliCredentialBuilder()
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+
+            //// VisualStudioCodeCredential
+            case VS_CODE:
+            VisualStudioCodeCredentialBuilder vStudioCodeCredentialBuilder = new VisualStudioCodeCredentialBuilder();
+            if (!JOrphanUtils.isBlank(tenantId)) {
+                vStudioCodeCredentialBuilder.tenantId(tenantId);
+            }
+            return vStudioCodeCredentialBuilder
+                .httpClient(AzUtilHttpClient.httpClientBase())
+                .build();
+        }
+        return null;
     }
 
     private String getKeyVaultProp(String secretName, String param) {
