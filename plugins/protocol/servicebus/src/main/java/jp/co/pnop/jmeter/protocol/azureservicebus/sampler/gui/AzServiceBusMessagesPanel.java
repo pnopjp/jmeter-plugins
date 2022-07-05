c7f4020a7689e086fe5ee065be80f40732928c33
Kuniteru Asami <kuniteru@asami.asia>
2022-07-06 06:17:50 +0900

    Let me change the order of the columns.
    ....| custom properties | label | -> ....| label | custom properties |

--- plugins/protocol/servicebus/src/main/java/jp/co/pnop/jmeter/protocol/azureservicebus/sampler/gui/AzServiceBusMessagesPanel.java
+++ plugins/protocol/servicebus/src/main/java/jp/co/pnop/jmeter/protocol/azureservicebus/sampler/gui/AzServiceBusMessagesPanel.java
@@ -20,8 +20,8 @@ public class AzServiceBusMessagesPanel extends AzAmqpMessagesPanel {
         tableModel = new ObjectTableModel(
             new String[] { COLUMN_NAMES.get("MESSAGE_TYPE"), COLUMN_NAMES.get("MESSAGE"), COLUMN_NAMES.get("MESSAGE_ID"), "session Id", "partition key", COLUMN_NAMES.get("CONTENT_TYPE"), COLUMN_NAMES.get("CUSTOM_PROPERTIES"), COLUMN_NAMES.get("LABEL") },
             AzAmqpMessage.class,
-            new Functor[] { new Functor("getMessageType"), new Functor("getMessage"), new Functor("getMessageId"), new Functor("getGroupId"), new Functor("getPartitionKey"), new Functor("getContentType"), new Functor("getCustomProperties"), new Functor("getLabel") },
-            new Functor[] { new Functor("setMessageType"), new Functor("setMessage"), new Functor("setMessageId"), new Functor("setGroupId"), new Functor("setPartitionKey"), new Functor("setContentType"), new Functor("setCustomProperties"), new Functor("setLabel") },
+            new Functor[] { new Functor("getMessageType"), new Functor("getMessage"), new Functor("getMessageId"), new Functor("getGroupId"), new Functor("getPartitionKey"), new Functor("getContentType"), new Functor("getLabel"), new Functor("getCustomProperties") },
+            new Functor[] { new Functor("setMessageType"), new Functor("setMessage"), new Functor("setMessageId"), new Functor("setGroupId"), new Functor("setPartitionKey"), new Functor("setContentType"), new Functor("setLabel"), new Functor("setCustomProperties") },
             new Class[] { String.class, String.class, String.class, String.class, String.class, String.class, String.class, String.class }
         );
     }
